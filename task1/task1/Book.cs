62f76b553d8a921b4b831d87cac4febcbc594b0d
Aniluffi <90216018+Aniluffi@users.noreply.github.com>
2024-04-18 20:23:14 +0300

    Add files via upload

---
+++ task1/task1/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Book:IComparison<Book>
+    {
+       public string Name { get; set; }
+       public int Coast { get; set; }
+
+        public Book(string name, int coast)
+        {
+            this.Name = name;
+            this.Coast = coast;
+        }
+
+        public Book()
+        {
+
+        }
+
+        public  List<Book> TypeComparison(List<Book> objects)
+        {
+            
+            return objects.OrderBy(book => book.Name).ToList(); ;
+        }
+    }
+}
---
+++ task1/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using task1;
+
+class Program
+{
+    public static void Main()
+    {
+        Book a = new Book();
+        List<Book> katalog = new List<Book>()
+        {
+       new Book("Дфра",12),
+       new Book("Карара",12),
+       new Book("Лали",12),
+       new Book("Ромбинзон круз",12),
+       new Book("Буратино",12)
+        };
+
+        katalog = a.TypeComparison(katalog);
+
+        foreach(Book book in katalog)
+        {
+            Console.WriteLine($"{book.Name}");
+        }
+    }
+}
